085baea407601d3f005cd6ba3226f442d37c0223
stv <stv@stanford.edu>
2018-08-27 12:04:14 -0700

    SQUASH: Cleanup include

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -3,7 +3,8 @@ URLs for the Enrollment API
 
 """
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include
+from django.conf.urls import patterns, url
 
 from .views import (
     EnrollmentView,
